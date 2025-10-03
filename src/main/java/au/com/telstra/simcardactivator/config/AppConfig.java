a4b882106cce618a55952a06704961b6308ef8a2
Brian Marcelino <hbrian83@gmail.com>
2025-10-03 14:50:21 +0300

    first commit

--- src/main/java/au/com/telstra/simcardactivator/config/AppConfig.java
+++ src/main/java/au/com/telstra/simcardactivator/config/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
+
     @Bean
     public SpringApplicationBuilder app() {
         return new SpringApplicationBuilder(SimCardActivator.class);
