18ebd3c9aa1c48ce607f6a53d42f3706e2408640
Brian Marcelino <hbrian83@gmail.com>
2025-10-03 17:14:26 +0300

    Add the Get API and Database

--- src/main/java/au/com/telstra/simcardactivator/controller/ActivationController.java
+++ src/main/java/au/com/telstra/simcardactivator/controller/ActivationController.java
@@ -1,16 +1,29 @@
 package au.com.telstra.simcardactivator.controller;
 
+import au.com.telstra.simcardactivator.dto.SimCardResponse;
 import au.com.telstra.simcardactivator.model.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import au.com.telstra.simcardactivator.repository.SimCardRecordRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
+
 @RestController
 public class ActivationController {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private static final Logger log = LoggerFactory.getLogger(ActivationController.class);
+
+    private final RestTemplate restTemplate;
+
+    private final SimCardRecordRepository repository;
+
+    public ActivationController(RestTemplate restTemplate, SimCardRecordRepository repository) {
+        this.restTemplate = restTemplate;
+        this.repository = repository;
+    }
 
     @PostMapping("/activate")
     public ResponseEntity<String> activateSim(@RequestBody SimCard request) {
@@ -30,4 +43,13 @@ public class ActivationController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Activation failed");
         }
     }
+
+    @GetMapping("/query")
+    public Object getSimRecord(@RequestParam Long simCardId) {
+        Optional<SimCardRecord> result = repository.findById(simCardId);
+
+        return result.map(r -> new SimCardResponse(r.getIccid(), r.getCustomerEmail(), r.isActive()))
+                .orElse(null); // come back here and change this to "Not found"
+    }
+
 }
