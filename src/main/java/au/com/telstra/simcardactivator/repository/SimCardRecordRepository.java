f787f2b58bd101a44fbcfb80cf4f38d47d9ee17f
LBJ236 <lbj236@localhost.localdomain>
2025-10-08 15:15:55 +0300

    Deleted

--- src/main/java/au/com/telstra/simcardactivator/repository/SimCardRecordRepository.java
+++
@@ -1,9 +0,0 @@
-// save, findbyId, findAll, deletebyId
-
-package au.com.telstra.simcardactivator.repository;
-
-import au.com.telstra.simcardactivator.model.SimCardRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SimCardRecordRepository  extends JpaRepository<SimCardRecord, Long> {
-}
